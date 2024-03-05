518e94ca38b9e648cfb21fa46c93d6d9630e84b4
jagger <1336098842@qq.com>
2024-03-05 19:39:03 +0800

    first commit

---
+++ lib/jagger_plerosis_test.rb
@@ -0,0 +1,6 @@
+require "jagger_plerosis_test/version"
+
+module JaggerPlerosisTest
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/jagger_plerosis_test/version.rb
@@ -0,0 +1,3 @@
+module JaggerPlerosisTest
+  VERSION = "0.1.1"
+end
